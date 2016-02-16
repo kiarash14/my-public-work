7ca233ef21c943afcc950eb9a88daa4c151dfce6
hacker44-h44 <hacker.ht44@gmail.com>
2016-02-16 20:13:09 +0330

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -228,7 +228,7 @@ function create_config( )
       "youtube",
       "media_handler",
       "moderation"},
-    sudo_users = {74889689},
+    sudo_users = { 172630253 ,  62834077 , 109005255 },
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'}
   }
